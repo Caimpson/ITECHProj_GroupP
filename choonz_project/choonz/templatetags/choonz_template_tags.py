af2874e716a69ab2465d5e4c1aa8e9d055b1ff36
Duncan Wright <1002243w@student.gla.ac.uk>
2020-02-17 20:17:37 +0000

    Separating code and documentation

--- choonz_project/choonz/templatetags/choonz_template_tags.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from choonz.models import Playlist
-
-register = template.Library()
-
-@register.inclusion_tag('choonz/categories.html')
-def get_playlist_list(current_playlist=None):
-    return {'categories': Playlist.objects.all(), 'current_playlist': current_playlist}
